8cd905cf23370be8a20f3ed1d7f26e2b54970ec8
John McGuckin <johnmcguckin@Johns-MacBook.local>
2024-11-29 12:48:48 +0000

    firebase connection started, need to fix imports

--- gs-spring-boot/complete/src/main/java/com/example/springboot/Application.java
+++ gs-spring-boot/complete/src/main/java/com/example/springboot/Application.java
@@ -1,29 +1,17 @@
 package com.example.springboot;
 
 import java.util.Arrays;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import java.io.FileInputStream;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.FirebaseApp;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-        FileInputStream serviceAccount = new FileInputStream("tourism-app-fd1ae-firebase-adminsdk-muvsn-8c79789f53");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
-
-        FirebaseApp.initializeApp(options);
-
-		SpringApplication.run(Application.class, args);
+	SpringApplication.run(Application.class, args);
 	}
 
 }
--- gs-spring-boot/complete/src/main/java/com/example/springboot/HelloController.java
+++ gs-spring-boot/complete/src/main/java/com/example/springboot/HelloController.java
@@ -3,17 +3,75 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnFailureListener;
 import java.util.*;
 
 @RestController
 public class HelloController {
+private Firestore db;
 
-    @GetMapping("/getDetails")
+public void setDb(Firestore db){
+    this.db = db;
+}
+
+public Firestore getDb(){
+    return this.db;
+}
+
+//     @GetMapping("/getDetails")
+//     @ResponseBody
+//     public Map<String, String> returnDetails(){
+//         HashMap<String, String> map = new HashMap<>();
+//         map.put("name", "His name is " + "John");
+//         return map;
+//
+
+    @GetMapping("/testDB")
     @ResponseBody
-    public Map<String, String> returnDetails(){
+    public void returnDetails(){
+    try{
+	 FileInputStream serviceAccount = new FileInputStream("tourism-app-fd1ae-firebase-adminsdk-muvsn-8c79789f53");
+
+   FirebaseOptions options = new FirebaseOptions.Builder()
+  .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+  .build();
+FirebaseApp.initializeApp(options);
+
+Firestore db = FirestoreClient.getFirestore();
+    setDb(db);
+
+	 }
+	 catch(Exception e){
+	 System.out.println(e);
+	 }
+
         HashMap<String, String> map = new HashMap<>();
         map.put("name", "His name is " + "John");
-        return map;
+
+        Firestore db = getDb();
+        db.collection("cities").document("LA")
+        .set(map)
+        .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+//                 Log.d(TAG, "DocumentSnapshot successfully written!");
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+//                 Log.w(TAG, "Error writing document", e);
+            }
+        });
     }
 
 }
